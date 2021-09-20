f3cd2106802bd98383fd3d69c4a316f1dad849c8
Gaz <d.galvre@gmail.com>
2021-09-19 19:47:20 -0500

    Cambios temporales para entender set y get

--- src/com/registroEmpleados/Empleado.java
+++ src/com/registroEmpleados/Empleado.java
@@ -1,14 +1,44 @@
 package com.registroEmpleados;
 
 public class Empleado {
-    String nombre;
-    String apellido;
+    private String nombre;
+    private String apellido;
+    private String fecha;
 
-    public Empleado(String nombre, String apellido){
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String n) {
+        if (!n.isEmpty())
+            nombre = n;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        String[] partes = nombreCompleto.split(" ");
+        this.nombre = partes[0];
+        this.apellido = partes[1];
+    }
+
+    public Empleado(String nombre, String apellido) {
         this.nombre = nombre;
         this.apellido = apellido;
     }
-    public void imprimirNombre(){
-        System.out.println("Nombre de empleado:" + nombre+ " " + apellido);
+
+    public void imprimirNombre() {
+        System.out.println("Nombre de empleado:" + nombre + " " + apellido);
     }
+}
+
+public class TestExample {
+    public static void main(String[] args) {
+        Empleado daniel = new Empleado("Daniel", "Galvez");
+        daniel.setNombre("Gabriela");
+        String nombreCompleto = "Gabriela Garcia";
+        String[] partes = nombreCompleto.split(" ");
+        if (!partes[0].isEmpty())
+            daniel.nombre = (partes[0]);
+        daniel.apellido = partes[1];
+    }
+
 }
